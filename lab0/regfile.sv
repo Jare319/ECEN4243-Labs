4d78db591fc0bd731b59d479102b807ec7d81f24
Jarrett <jarrettcrump@gmail.com>
2023-01-28 13:32:34 -0600

    Added some comments

--- lab0/regfile.sv
+++ lab0/regfile.sv
@@ -21,9 +21,9 @@ module regfile (input logic clock,
    // register 0 hardwired to 0
 
    always @(posedge clock) begin
-      if (wenable && waddr != 'b00000)
+      if (wenable && waddr != 'b00000) // Writes to the write address as long as the enable is high, and write address isn't register 0
          rf[waddr] = wdata;
-      rdata1 = rf[raddr1];
+      rdata1 = rf[raddr1]; // Output ports show value of register with address of 'read addresses' 1 and 2
       rdata2 = rf[raddr2];
    end
          
