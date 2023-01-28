3dfd75f985ddcbf7662e45cd56c133fa4972af17
Jarrett <you@example.com>
2023-01-27 18:11:26 -0600

    Waveform output from Vsim

--- lab0/regfile_tb.sv
+++ lab0/regfile_tb.sv
@@ -3,6 +3,7 @@ module stimulus ();
      logic clock, wenable;
      logic [4:0] raddr1, raddr2, waddr;
      logic [31:0] wdata, rdata1, rdata2;
+     
    
    // Instantiate DUT
    regfile r (clock, wenable, raddr1, raddr2, waddr, wdata, rdata1, rdata2);
@@ -14,27 +15,23 @@ module stimulus ();
 	forever #5 clock = ~clock;
      end
 
-   initial
-     begin
-	// Gives output file name
-	handle3 = $fopen("regfile_test.out");
-	// Tells when to finish simulation
-	#500 $finish;		
-     end 
    
    initial 
      begin      
 	#0 wenable = 0;
      #0 waddr = 'b00001;
-     #0 wdata = 'h02A03C30; // Tests the write-enable
-     #20 wenable = 1;
-     #0 wdata = 'hFFFFFFFF; // Tests writing in general
-     #30 waddr = 'b00000;
+     #0 wdata = 'h02A03C30;
+     #0 raddr1 = 'b00001; // Tests the write-enable
+     #15 wenable = 1;
+     #5 wdata = 'hFFFFFFFF;
+     #0 raddr1 = 'b00001; // Tests writing in general
+     #20 waddr = 'b00000;
      #0 wdata = 'hFFFFFFFF;
-     #0 raddr1 = 'b00001; // tests writing to reg 0 and reading from an address
-     #40 waddr = 'b00010;
+     #0 raddr1 = 'b00000; // tests writing to reg 0 and reading from an address
+     #20 waddr = 'b00010;
      #0 wdata = 'hFFFFFFFF;
-     #50 wdata = 'h0000000;
+     #0 raddr2 = 'b00010;
+     #20 wdata = 'h0000000;
      #0 raddr2 = 'b00010; // Tests reading from an address as it is being re-writen
 
      end
