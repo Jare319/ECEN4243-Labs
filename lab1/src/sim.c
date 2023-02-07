874d21d6c308f28af3cca83563900f2b6a03fa57
Brax_The_Builder <backley@okstate.edu>
2023-02-07 12:27:15 -0600

    Added J-type to sim.c and jalr to I-type

--- lab1/src/sim.c
+++ lab1/src/sim.c
@@ -235,6 +235,12 @@ int i_process(char* i_) {
   //   return 0;
   // }
 
+  // if(!strcmp(d_opcode,"1100111") && !strcmp(funct3,"000")) {
+  //   printf("--- This is an JALR instruction. \n");
+  //   JALR(Rd, Rs1, Imm, Funct3);
+  //   return 0;
+  // }
+
   /*These 5 use Imm(rs1), not sure what that is*/
 
   // if(!strcmp(d_opcode,"0000011") && !strcmp(funct3,"000")) {
@@ -398,7 +404,52 @@ int j_process(char* i_) {
 
   /* This function execute Jump instructions */
 
-  /* Add jump instructions here */ 
+  char d_opcode[8];
+  d_opcode[0] = i_[31-6]; 
+  d_opcode[1] = i_[31-5]; 
+  d_opcode[2] = i_[31-4]; 
+  d_opcode[3] = i_[31-3];
+  d_opcode[4] = i_[31-2]; 
+  d_opcode[5] = i_[31-1]; 
+  d_opcode[6] = i_[31-0]; 
+  d_opcode[7] = '\0';
+  char rd[6]; rd[5] = '\0';
+  char imm[21]; 
+
+  imm[0] = i_[31-]; 
+  imm[1] = i_[31-]; 
+  imm[2] = i_[31-]; 
+  imm[3] = i_[31-];
+  imm[4] = i_[31-]; 
+  imm[5] = i_[31-]; 
+  imm[6] = i_[31-];
+  imm[7] = i_[31-];
+  imm[8] = i_[31-];
+  imm[9] = i_[31-];
+  imm[10] = i_[31-];
+  imm[11] = i_[31-];
+  imm[12] = i_[31-];
+  imm[13] = i_[31-];
+  imm[14] = i_[31-];
+  imm[15] = i_[31-];
+  imm[16] = i_[31-];
+  imm[17] = i_[31-];
+  imm[18] = i_[31-];
+  imm[19] = i_[31-];
+  imm[20] = i_[31-];
+  imm[21] = '\0';  
+
+  int Rd = bchar_to_int(rd);
+  int Imm = bchar_to_int(imm);
+  printf ("Opcode = %s\n Imm = %d\n Rd = %d\n",
+	  d_opcode, Imm, Rd);
+  printf("\n");
+
+  // if(!strcmp(d_opcode,"1101111")) {
+  //   printf("--- This is a JAL instruction. \n");
+  //   JAL(Rd, Imm);
+  //   return 0;
+  // }
 
   return 1;
 
