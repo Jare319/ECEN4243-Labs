feb9e6239831196649ebf6abceb35b3d88a21af0
Jarrett <you@example.com>
2023-03-06 21:29:50 -0600

    xor(i) implementation and test

--- lab2/hdl/riscv_single.sv
+++ lab2/hdl/riscv_single.sv
@@ -40,7 +40,7 @@ module testbench();
    initial
      begin
 	string memfilename;
-        memfilename = {"../riscvtest/riscvtest.memfile"};
+        memfilename = {"../riscvtest/xor(i)-test.memfile"};
         $readmemh(memfilename, dut.imem.RAM);
      end
 
