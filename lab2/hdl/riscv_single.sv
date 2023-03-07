d7d8c853e2c63218ed6501e5e16cd4ecbd178e66
Jarrett <you@example.com>
2023-03-06 20:23:52 -0600

    xor/xori implementation

--- lab2/hdl/riscv_single.sv
+++ lab2/hdl/riscv_single.sv
@@ -162,6 +162,7 @@ module aludec (input  logic       opb5,
 		  else
 		    ALUControl = 3'b000; // add, addi
 		  3'b010: ALUControl = 3'b101; // slt, slti
+      3'b100: ALUControl = 3'b100; // xor, xori
 		  3'b110: ALUControl = 3'b011; // or, ori
 		  3'b111: ALUControl = 3'b010; // and, andi
 		  default: ALUControl = 3'bxxx; // ???
@@ -324,7 +325,8 @@ module alu (input  logic [31:0] a, b,
        3'b001:  result = sum;         // subtract
        3'b010:  result = a & b;       // and
        3'b011:  result = a | b;       // or
-       3'b101:  result = sum[31] ^ v; // slt       
+       3'b101:  result = sum[31] ^ v; // slt
+       3'b100:  result = a ^ b;       // xor
        default: result = 32'bx;
      endcase
 
