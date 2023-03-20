7c6d58bacd356c6fd630a72da07b7a2de2d4fcab
Brax_The_Builder <backley@okstate.edu>
2023-03-20 18:16:58 -0500

    maindec fix

--- lab2/hdl/riscv_single.sv
+++ lab2/hdl/riscv_single.sv
@@ -144,14 +144,14 @@ module maindec (input  logic [6:0] op,
    always_comb
      case(op)
        // RegWrite_ImmSrc_ALUSrc_MemWrite_ResultSrc_Branch_ALUOp_Jump
-       7'b0000011: controls = 11'b1_000_1_0_01_0_00_0; // lw
-       7'b0100011: controls = 11'b0_001_1_1_00_0_00_0; // sw
-       7'b0110011: controls = 11'b1_xxx_0_0_00_0_10_0; // R–type
-       7'b1100011: controls = 11'b0_010_0_0_00_1_01_0; // beq/bne
-       7'b0010011: controls = 11'b1_000_1_0_00_0_10_0; // I–type ALU
-       7'b1101111: controls = 11'b1_011_0_0_10_0_00_1; // jal
-       7'b0110111: controls = 11'b1_100_1_0_00_0_00_0; // U-type
-       default: controls = 11'bx_xx_x_x_xx_x_xx_x; // ???
+       7'b0000011: controls = 12'b1_000_1_0_01_0_00_0; // lw
+       7'b0100011: controls = 12'b0_001_1_1_00_0_00_0; // sw
+       7'b0110011: controls = 12'b1_xxx_0_0_00_0_10_0; // R–type
+       7'b1100011: controls = 12'b0_010_0_0_00_1_01_0; // beq/bne
+       7'b0010011: controls = 12'b1_000_1_0_00_0_10_0; // I–type ALU
+       7'b1101111: controls = 12'b1_011_0_0_10_0_00_1; // jal
+       7'b0110111: controls = 12'b1_100_1_0_00_0_00_0; // U-type
+       default: controls = 12'bx_xxx_x_x_xx_x_xx_x; // ???
      endcase // case (op)
    
 endmodule // maindec
