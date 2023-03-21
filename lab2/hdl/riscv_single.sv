ab6c94fee6bd59a0b60ae89fbc6de5e219386dbf
Brax_The_Builder <backley@okstate.edu>
2023-03-21 18:04:48 -0500

    edit

--- lab2/hdl/riscv_single.sv
+++ lab2/hdl/riscv_single.sv
@@ -147,7 +147,7 @@ module maindec (input  logic [6:0] op,
        7'b0000011: controls = 12'b1_000_1_0_01_0_00_0; // lw
        7'b0100011: controls = 12'b0_001_1_1_00_0_00_0; // sw
        7'b0110011: controls = 12'b1_xxx_0_0_00_0_10_0; // R–type
-       7'b1100011: controls = 12'b0_010_0_0_00_1_01_0; // beq/bne
+       7'b1100011: controls = 12'b0_010_0_0_00_1_01_0; // B-type
        7'b0010011: controls = 12'b1_000_1_0_00_0_10_0; // I–type ALU
        7'b1101111: controls = 12'b1_011_0_0_10_0_00_1; // jal
        7'b0110111: controls = 12'b1_100_1_0_00_0_00_0; // U-type
